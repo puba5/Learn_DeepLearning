57d52f01c31e5a770061557ccea341f7e8274fa6
WooYoo <puba5@naver.com>
2020-03-07 20:34:58 +0900

    perceptron 완료, dataset 폴더 및 파일 추가, mnist.py 추가

--- perceptron.py
+++ perceptron.py
@@ -1,10 +1,57 @@
 import numpy as np
-x = np.array([0, 1])
+import matplotlib.pylab as plt
+
+
+#계단  함수
+def step_function(x):
+    return np.array(x > 0, dtype=np.int)
+
+
+#시그모이드 함수
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+#ReLU 함수
+def relu(x):
+    return np.maximum(0, x)
+
+
+'''
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = sigmoid(x)
+plt.plot(x, y)
+y = step_function(x)
+plt.plot(x, y)
+y = relu(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1)
+plt.show()
+'''
+
+A = np.array([[1, 2], [3, 4]])
+A.shape
+B = np.array([[5, 6], [7, 8]])
+B.shape
+print(np.dot(A, B))
+
+
+#x = np.array([-1.0, 1.0, 2.0])
+#print(sigmoid(x))
+
+#y = x > 0
+#print(y.astype(np.int))
+
+
+'''
 w = np.array([0.5, 0.5])
 b = -0.7
 
+
 print(w*x)
 print(np.sum(w*x)+b)
+'''
 
 '''
 AND 게이트 구현
@@ -63,5 +110,20 @@ def XOR(x1, x2):
     return y
 
 
-print(XOR(0, 0))
+x = np.array([1.0, 0.5])
+W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+B1 = np.array([0.1, 0.2, 0.3])
 
+print(W1.shape)  # (2,3)
+print(x.shape)
+print(B1.shape)
+
+A1 = np.dot(x, W1) + B1
+print(A1)
+Z1 = sigmoid(A1)
+print(Z1)
+
+W2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+B2 = np.array([0.1, 0.2])
+
+print(Z1.shape)
